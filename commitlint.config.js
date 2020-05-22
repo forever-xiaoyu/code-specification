fea31d109f3cae79ae6dc4a1ccee4bec7fcfeaa7
于策 <yuce@ZBMAC-C02VQ075W.local>
2020-05-22 19:58:58 +0800

    特性: 整体项目配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,8 @@
 "use strict";
 
 module.exports = {
-  extends: ["@commitlint/config-conventional"]
+  // 符合Angular风格的校验规则
+  // 使用该校验规则不能对.cz-config.js进行不符合Angular规范的定制处理，例如汉化
+  // extends: ["@commitlint/config-conventional"]
+  extends: ['cz']
 };
