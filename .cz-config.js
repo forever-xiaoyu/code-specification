402f0aad0248f2ecd717c4932b7db9471620392b
于策 <yuce@ZBMAC-C02VQ075W.local>
2020-05-26 17:31:12 +0800

    feat: commitizen 配置

--- .cz-config.js
+++ .cz-config.js
@@ -3,15 +3,15 @@
 module.exports = {
 
   types: [
-    {value: 'feat',     name: '特性:    一个新的特性'},
-    {value: 'fix',      name: '修复:    修复一个Bug'},
-    {value: 'docs',     name: '文档:    变更的只有文档'},
-    {value: 'style',    name: '格式:    空格, 分号等格式修复'},
-    {value: 'refactor', name: '重构:    代码重构，注意和特性、修复区分开'},
-    {value: 'perf',     name: '性能:    提升性能'},
-    {value: 'test',     name: '测试:    添加一个测试'},
-    {value: 'chore',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
-    {value: 'revert',   name: '回滚:    代码回退'}
+    {value: 'feat',     name: 'feat:     一个新的特性'},
+    {value: 'fix',      name: 'fix:      修复一个Bug'},
+    {value: 'docs',     name: 'docs:     变更的只有文档'},
+    {value: 'style',    name: 'style:    空格, 分号等格式修复'},
+    {value: 'refactor', name: 'refactor: 代码重构，注意和特性、修复区分开'},
+    {value: 'perf',     name: 'perf:     提升性能'},
+    {value: 'test',     name: 'test:     添加一个测试'},
+    {value: 'chore',    name: 'chore:    开发工具变动(构建、脚手架工具等)'},
+    {value: 'revert',   name: 'revert:   代码回退'}
   ],
 
   scopes: [
